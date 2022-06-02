a703b47cb81f3f4d4a4d399755b7a0016162584e
Lennon Moisés Sánchez Freites <moisesfreites3@gmail.com>
2022-06-02 19:47:27 -0400

    Conectando formulario de contacto, prueba uno

--- src/Components/ContactForm.tsx
+++ src/Components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import React, { FormEvent, useRef } from 'react'
 import toast from 'react-hot-toast';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -19,29 +19,24 @@ const ContactForm = () => {
         resolver: yupResolver(validations)
     });
 
-    const handleSend = (data: FormData) => {
-        const formData = new FormData()
-        formData.append("name", data.name)
-        formData.append("email", data.email)
-        formData.append("message", data.message)
-
+    const handleSend = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
         fetch("/", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: new URLSearchParams(formData.toString()),
-        }).then(() => {
-            toast.success("Your message was send successfully");
-        }).catch(error => {
-            console.log("error al enviar correo", error);
-            toast.error("Error to send your message, sorry. Try later");
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(form.toString()),
         })
-    };
+            .then(() => toast.success("Form successfully submitted"))
+            .catch((error) => {
+                console.log("error al enviar correo", error)
+                toast.error("Error to send form, try later.")
+            });
+    }
 
     return (
         <section className="w-full h-screen p-10 text-white flex flex-col justify-center items-center space-y-10 text-justify">
-            <form ref={formRef} onSubmit={handleSubmit(handleSend)} className="w-full lg:w-3/5 min-h-4/5 max-h-screen rounded-lg grid grid-cols-1 grid-rows-6 gap-5 text-center p-5 lg:p-10 text-base lg:text-lg border border-[#3178c6]">
+            <form ref={formRef} id="formRef" onSubmit={handleSend} data-netlify="true" className="w-full lg:w-3/5 min-h-4/5 max-h-screen rounded-lg grid grid-cols-1 grid-rows-6 gap-5 text-center p-5 lg:p-10 text-base lg:text-lg border border-[#3178c6]">
                 <h1 className="text-violet text-4xl lg:text-6xl" >Contact me</h1>
                 <input type="hidden" name="form-name" value="contact" />
                 <div className="w-full h-full">
