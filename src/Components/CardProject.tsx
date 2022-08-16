f51c759f7be9cbeaf71e055f112dd0ac851b7add
Lennon Moisés Sánchez Freites <moisesfreites3@gmail.com>
2022-08-16 18:37:05 -0400

    Ajuste en el alt de la imagen de cardProject

--- src/Components/CardProject.tsx
+++ src/Components/CardProject.tsx
@@ -10,7 +10,7 @@ const CardProject = ({ imageSource, title, description, link }: PropsItemCardGri
         <section className="w-full h-90 lg:h-80vh max-h-[90vh] flex justify-center items-center">
             <div className="card">
                 <div className="card2 rounded-lg p-5 flex flex-col justify-start items-center space-y-10 lg:space-y-5">
-                    <img src={imageSource} alt="image-project-icon" className="rounded-lg w-auto max-h-[50%]" />
+                    <img src={imageSource} alt="project-icon" className="rounded-lg w-auto max-h-[50%]" />
                     <div className="h-full grid gap-y-5">
                         <label className="text-violet text-2xl md:text-3xl">
                             <a href={link} target="_blank" rel="noreferrer">
