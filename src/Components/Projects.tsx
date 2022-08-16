f4338c8d292f755be96978dcceb43cbd70cd3d2a
Lennon Moisés Sánchez Freites <moisesfreites3@gmail.com>
2022-08-16 18:31:59 -0400

    Ajuste cardProjects

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -10,13 +10,13 @@ const Projects = () => {
 
             <div className="w-full rounded-lg grid place-content-center gap-y-5 lg:gap-5 grid-cols-1 lg:grid-cols-3 text-center text-base lg:text-lg">
 
-                <CardProject image={images.expenditure_recording} title="Expenditure recording app" description='App to keep a list of expenses. It is initialized with an initial balance, as expenses are added they are subtracted from that balance and the percentage spent is shown in the graph. You can delete, edit and filter by category each expense.' link="https://recording-expediture.netlify.app/" />
+                <CardProject imageSource={images.expenditure_recording} title="Expenditure recording app" description='App to keep a list of expenses. It is initialized with an initial balance, as expenses are added they are subtracted from that balance and the percentage spent is shown in the graph. You can delete, edit and filter by category each expense.' link="https://recording-expediture.netlify.app/" />
 
-                <CardProject image={images.naruto_memory_game} title="Naruto memory app" description='Challenge placed in the community. It consists of solving a memory game, with the desired theme, in a set time.' link="https://amazing-naruto-memory-game.netlify.app/" />
+                <CardProject imageSource={images.naruto_memory_game} title="Naruto memory app" description='Challenge placed in the community. It consists of solving a memory game, with the desired theme, in a set time.' link="https://amazing-naruto-memory-game.netlify.app/" />
 
-                <CardProject image={images.weather_search} title="Weather search app" description='Consumption of a Rest API that returns weather data according to country and city selected.' link='https://weather-search-cities-app.netlify.app/' />
+                <CardProject imageSource={images.weather_search} title="Weather search app" description='Consumption of a Rest API that returns weather data according to country and city selected.' link='https://weather-search-cities-app.netlify.app/' />
 
-                <CardProject image={images.find_lyrics} title="Search songs lyrics" description='Search for song lyrics by artist and song name. A Rest API is consumed.' link="https://find-lyrics-app.netlify.app/" />
+                <CardProject imageSource={images.find_lyrics} title="Search songs lyrics" description='Search for song lyrics by artist and song name. A Rest API is consumed.' link="https://find-lyrics-app.netlify.app/" />
             </div>
 
             <img src={images.pritingSoftIcon} alt="profile-icon" className="w-16 lg:w-20 h-16 lg:h-20" />
@@ -24,7 +24,7 @@ const Projects = () => {
 
             <div className="w-full rounded-lg grid place-content-center gap-y-5 lg:gap-5 grid-cols-1 lg:grid-cols-3 text-center text-base lg:text-lg">
 
-                <CardProject image={images.feeseAppIcon} title="Feese App" description='PWA for keeping records of production and payment of hair removal employees.' link="https://drive.google.com/file/d/1YRZDUmrLc9gtrEPJ_H5i4-Fl7Y1EpZo5/view?usp=sharing" />
+                <CardProject imageSource={images.feeseAppIcon} title="Feese App" description='PWA for keeping records of production and payment of hair removal employees.' link="https://drive.google.com/file/d/1YRZDUmrLc9gtrEPJ_H5i4-Fl7Y1EpZo5/view?usp=sharing" />
             </div>
 
         </section>
